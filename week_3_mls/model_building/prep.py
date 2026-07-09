1e298c1f6998ff66a83388b41f0b7ecf7d2ab790
ebolwan <pitbw15@gmail.com>
2026-07-09 16:35:58 +0000

    Add Week 3 MLS pipeline

--- week_3_mls/model_building/prep.py
+++ week_3_mls/model_building/prep.py
@@ -48,4 +48,4 @@ for file_path in files:
         path_in_repo=file_path.split("/")[-1],  # just the filename
         repo_id="bolwelwan/machine-failure-prediction",
         repo_type="dataset",
-    )
+    ) 
